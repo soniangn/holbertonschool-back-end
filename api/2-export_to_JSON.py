db98e7ada132385723aca74b2c198cf7f6e9ad98
soniangn <nguyen.sonia@gmail.com>
2023-04-05 08:14:36 -0700

    done task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+uses an API and for a given employee ID,
+returns information about his/her TODO list progress.
+export data in the JSON format.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+
+    USER_ID = sys.argv[1]
+    url_id = f'{url}/users/{USER_ID}'
+    api_response_employee = requests.get(url_id)
+    employee_json = api_response_employee.json()
+    USERNAME = employee_json.get('username')
+
+    url_tasks = f'{url}/todos?userId={USER_ID}'
+    api_response_tasks = requests.get(url_tasks)
+    task_json = api_response_tasks.json()
+
+    tasks = []
+    for task in task_json:
+        dict_tasks = {"task": task.get('title'),
+                 "completed": task.get('completed'), "username": USERNAME}
+        tasks.append(dict_tasks)
+
+    all_tasks = {USER_ID: tasks}
+
+    with open(f'{USER_ID}.json', 'w') as json_file:
+        json.dump(all_tasks, json_file)
