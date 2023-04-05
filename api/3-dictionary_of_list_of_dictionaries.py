2aa3ef2d37cf3e48d156fb0896cdaf0338654f33
soniangn <nguyen.sonia@gmail.com>
2023-04-05 09:47:54 -0700

    try to check task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+uses an API and for a given employee ID,
+returns information about his/her TODO list progress.
+export data in the JSON format.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    url_id = f"{url}/users"
+    api_response_id = requests.get(url_id)
+    id_json = api_response_id.json()
+    
+    employees_tasks = {}
+    employee_dict = []
+    for person in id_json:
+        id = person.get('id')
+        username = person.get('username')
+        url_tasks = f'{url}/todos?userId={id}'
+        api_response_tasks = requests.get(url_tasks)
+        tasks_json = api_response_tasks.json()
+        
+        for task in tasks_json:
+            employee_tasks = {"username": username, 
+                              "task": task.get('title'),
+                              "completed": task.get('completed')}
+            employee_dict.append(employee_tasks)
+        employees_tasks[id] = employee_dict
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(employees_tasks, json_file)
